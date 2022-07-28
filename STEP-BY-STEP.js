baaa38a724b13a18b37c7fbd5e09fceff98ce5b6
Farhod <farhodbozarov19752210@gmail.com>
2022-07-28 16:50:46 +0500

    proekt tugadi

--- STEP-BY-STEP.js
+++ STEP-BY-STEP.js
@@ -6,8 +6,10 @@ let d = (div) => document.createElement(div);
 let arr1 = [];
 let massiv = [];
 let massiv1 = [];
-
+let hh = 0;
+let n = 4;
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
 function shuffle(array) {
   let currentIndex = array.length,
     randomIndex;
@@ -22,31 +24,37 @@ function shuffle(array) {
   return array;
 }
 shuffle(arr);
-console.log(arr);
 
-let hh = 0;
 for (let i = 0; i < 16; i++) {
   let block = d("div");
   block.className = "block";
   massiv.push(block);
-  block.onclick = function () {
-    block.classList.add("colored");
-    massiv1.push(i + 1);
-    arr1.push(arr[hh]);
-    if (massiv1[hh] === arr1[hh] && hh === 4) {
-      setTimeout(() => {
-        alert("Siz g'alaba qozondingiz!!!");
-        document.location.reload(true);
-      });
-    } else if (massiv1[hh] !== arr[hh]) {
-      block.style.backgroundColor = "red";
-      setTimeout(() => {
-        alert("Siz mag'lub bo'ldingiz");
-        document.location.reload(true);
-      });
-    }
-    hh++;
-  };
+  setTimeout(() => {
+    block.onclick = function () {
+      block.classList.add("colored");
+      massiv1.push(i + 1);
+      arr1.push(arr[hh]);
+      if (massiv1[hh] === arr1[hh] && hh === n - 1) {
+        n++;
+        setTimeout(() => {
+          alert("Siz g'alaba qozondingiz!!!");
+          massiv.forEach((element) => element.classList.remove("colored"));
+          start();
+          massiv1 = [];
+          arr1 = [];
+          arr = shuffle(arr);
+          hh = 0;
+        });
+      } else if (massiv1[hh] !== arr[hh]) {
+        block.style.backgroundColor = "red";
+        setTimeout(() => {
+          alert("Siz mag'lub bo'ldingiz");
+          document.location.reload(true);
+        });
+      }
+      hh++;
+    };
+  }, n * 1000 + 1000);
   game.appendChild(block);
 }
 
@@ -55,7 +63,7 @@ const start = () => {
   let time = setInterval(() => {
     massiv[arr[h] - 1].classList.add("colored");
     h += 1;
-    if (h == 5) {
+    if (h == n) {
       clearInterval(time);
       setTimeout(() => {
         massiv.forEach((element) => element.classList.remove("colored"));
@@ -64,4 +72,3 @@ const start = () => {
   }, 1000);
 };
 start();
-console.log();
